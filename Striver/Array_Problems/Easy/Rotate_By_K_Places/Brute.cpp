f8685ab0c5b91c1ab0bf374708c27e3f8460cf9b
Aashish187 <aashishsinghnegi0408@gmail.com>
2026-02-15 00:26:26 +0530

    rotate array by k places

---
+++ Striver/Array_Problems/Easy/Rotate_By_K_Places/Brute.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void left_rotated(vector<int>& arr, int n){
+/*
+    Since we just have to left rotate the array by one, we will take the first element and store it in
+    temporary variable then loop and shift all the elements to the left and then assign the first variable to 
+    the last element
+Time complexity:O(n*k) 
+Space Complexity:O(1)
+*/
+    int temp=arr[0];
+    for(int j =1;j<n;j++){
+        arr[j-1]=arr[j];
+    }
+    arr[n-1]=temp;
+}
+int main()
+{
+    int n ;
+    cout<<"Enter the number of elements"<<endl;
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter the array elements: "<<endl;
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+    int place;
+    cout<<"Enter the Number Of places to be shifted: "<<endl;
+    cin>>place;
+    while(place>0){
+    left_rotated(arr,n);
+    place --;
+    }
+    for(int x : arr){
+        cout<<x<<" ";
+    }
+    return 0;
+}
