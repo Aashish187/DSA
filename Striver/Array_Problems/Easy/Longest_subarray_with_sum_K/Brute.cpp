9741fd32804b64a6342adf46d4d54b93ac78d6ea
Aashish187 <aashishsinghnegi0408@gmail.com>
2026-05-16 15:16:11 +0530

    Brute Approach

---
+++ Striver/Array_Problems/Easy/Longest_subarray_with_sum_K/Brute.cpp
@@ -0,0 +1,49 @@
+/*
+TIME COMPLEXITY : O(n**3)
+SPACE COMPLEXITY : O(1)
+
+ALGORITHM : 1) make all the subarrays from starting to end with two loops
+            2) use the third loop to calculate the sum form i to j
+            3) check for the sum if it is equal return the length of subarray
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int longest_subarray(vector<int> &arr, int n, int x)
+{
+    int length = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int sum = 0;
+            for (int k = i; k <= j; k++)
+            {
+                sum += arr[k];
+            }
+            if (sum == x)
+            {
+                length = max(length, j - i + 1);
+            }
+        }
+    }
+    return length;
+}
+int main()
+{
+    int n;
+    cout << "Enter the Number Of Elements in the Array: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int k;
+    cout << "Enter the Sum of the subarray: ";
+    cin >> k;
+    cout << "Maximum length of the Subarray with Sum k: " << longest_subarray(arr, n, k);
+
+    return 0;
+}
