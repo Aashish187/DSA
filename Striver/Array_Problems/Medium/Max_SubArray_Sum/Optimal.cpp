5534630fa8832caaed0a226014263c8dc896227f
Aashish187 <aashishsinghnegi0408@gmail.com>
2026-06-10 01:04:47 +0530

    Formatted

--- Striver/Array_Problems/Medium/Max_SubArray_Sum/Optimal.cpp
+++ Striver/Array_Problems/Medium/Max_SubArray_Sum/Optimal.cpp
@@ -5,8 +5,8 @@ Algorithm  :   KADANE ALGORITHM
             1) Initialize 'max_sum' with INT_MIN and a running 'sum' as 0.
             2) Traverse the array, adding each element to the running 'sum'.
             3) After each addition, update 'max_sum' if the current 'sum' is greater.
-            4) The "Magic Step": If the running 'sum' drops below 0, reset it to 0. 
-               This is because a negative sum will always reduce the value 
+            4) The "Magic Step": If the running 'sum' drops below 0, reset it to 0.
+               This is because a negative sum will always reduce the value
                of any future subarray it's added to.
             5) Return the final 'max_sum'.
 */
@@ -15,12 +15,14 @@ using namespace std;
 int Max_Subarray_Sum(vector<int> &arr, int n)
 {
     int max_sum = INT_MIN;
-    int sum  = 0;
-    for (int i = 0; i < n; i++){
-        sum+=arr[i];
-        max_sum=max(max_sum, sum);
-        if(sum<0){
-            sum=0;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        max_sum = max(max_sum, sum);
+        if (sum < 0)
+        {
+            sum = 0;
         }
     }
     return max_sum;
